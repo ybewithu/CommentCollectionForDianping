63308c8e16ed1ae18f62bce007fb7ed9ff6f36fb
ybewithu <52787068+ybewithu@users.noreply.github.com>
2020-05-28 19:52:37 +0800

    Add files via upload

---
+++ park1.py
@@ -0,0 +1,119 @@
+# coding=utf-8
+from bs4 import BeautifulSoup
+import sys
+import json
+import requests
+import time
+import random
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+
+returnList = []
+
+USER_AGENTS = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
+]
+
+cookies = "_lxsdk_cuid=167a5bba59b65-0aff49d74d330b-6313363-144000-167a5bba59cc8; _lxsdk=167a5bba59b65-0aff49d74d330b-6313363-144000-167a5bba59cc8; _hc.v=22118c09-011e-9174-180f-e852e8c314ef.1544673798; dper=2959c79e1748a6c673435433ff492a11d0d92b5fc4b136f952e7230ea585e60588b73f6f28101acee0c3d2dee1654c3e2e9b1a36785e8539b230b3d9d1706c706088d89d536b44c119854c96673d03b2e48d7a96ee63c262b1ab25c5036db1d7; ll=7fd06e815b796be3df069dec7836c3df; ua=dpuser_7599353634; ctu=17b71a9f3a87101957d627f08b1cd40b06f2e6153997998ad6a6a1e24297b621; _lxsdk_s=167a6d306d3-88c-292-2c9%7C%7C889"
+
+
+header = {
+    'User-Agent': random.choice(USER_AGENTS),
+    'Cookie': cookies,
+    'Referer': 'http://www.dpfile.com/app/pc-common/index.min.30301999a81455aaf8a16973b3b13888.css'
+}
+
+
+requests.adapters.DEFAULT_RETRIES = 5
+s = requests.session()
+s.keep_alive = False
+
+url = "http://www.dianping.com/shop/17793611/review_all"
+r = requests.get(url, headers=header)
+time.sleep(random.random())
+print(r.status_code)
+#print(r.text)
+
+soup = BeautifulSoup(r.text, 'lxml')
+# print length
+lenth = soup.find_all(class_='PageLink').__len__() + 1
+
+file = open(r"C:\Users\user\Desktop\park1.txt", "w")
+for i in range(179):
+    urlIn = "http://www.dianping.com/shop/17793611/review_all/p%s" % (i+1)
+    re = requests.get(urlIn, headers=header)
+    time.sleep(random.random()*8)
+    soupIn = BeautifulSoup(re.text, 'lxml')
+    comment = soup.select('.reviews-items ul li')
+    for one in comment:
+        try:
+            if one['class'][0] == 'item':
+                continue
+        except:
+            pass
+        print("hello")
+        name = one.select_one('.main-review .dper-info .name')
+        # print name.get_text().strip()
+        name = name.get_text().strip()
+        star = one.select_one('.main-review .review-rank span')
+        # print star['class'][1][7:8]
+        star = star['class'][1][7:8]
+        pl = one.select_one('.main-review .review-words')
+        pl['class'] = {'review-words'}
+        words = pl.get_text().strip()
+        returnList.append([name, star, words])
+
+
+print(returnList)
+cnt = 0
+for one in returnList:
+    try:
+        cnt += 1
+        print("No." + str(1) + "comment")
+        # print(one)
+        file.write("\n")
+        file.write(str(one[0]))
+        file.write("\n")
+        file.write(str(one[1]))
+        file.write("\n")
+        file.write(str(one[2]))
+        file.write("\n")
+
+    except:
+        continue
+file.close()
+
